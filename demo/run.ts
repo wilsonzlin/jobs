98bc43dc8156bc0ae95363a965f88b0981c65ce5
Wilson Lin <code@wilsonl.in>
2020-06-11 20:01:51 +1000

    Create demo

---
+++ demo/run.ts
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const jobs = require('@wzlin/jobs');
+const mkdirp = require('mkdirp');
+const path = require('path');
+
+const BUILD = path.join(__dirname, 'build');
+mkdirp.sync(BUILD);
+
+const COMPANIES = [
+  // 'Amazon',
+  'Facebook',
+  // 'Google',
+  // 'Microsoft',
+];
+
+(async () => {
+  const rawJobs = await jobs.fetchAndParse({
+    cacheDir: path.join(__dirname, 'cache'),
+    companies: COMPANIES,
+  });
+
+  await fs.promises.writeFile(path.join(BUILD, 'jobs.json'), JSON.stringify(rawJobs, null, 2));
+})()
+  .catch(console.error);
